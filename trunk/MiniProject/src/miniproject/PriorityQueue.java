10d0ff7a156caa790d49cbbcdebba16be1e06e71
stauffkt.dchi <stauffkt.dchi@66a29046-b2a1-11de-8af0-9d1a786950a5>
2009-10-12 20:46:27 +0000

    Documentation is now included.

--- trunk/MiniProject/src/miniproject/PriorityQueue.java
+++ trunk/MiniProject/src/miniproject/PriorityQueue.java
@@ -1,25 +1,35 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package miniproject;
 import java.util.ArrayList;
 
 /**
- *
- * @author Raymond Cox <rj.cox101 at gmail.com>
- * @author Ryan Cummins
+ * The Priority Queue class is an array organized into a binary tree.  It has
+ * items in the array that have a character array and a priority value.
  */
 public class PriorityQueue {
     ArrayList<PriorityItem> _items = new ArrayList();
 
+    /**
+     * Creates a new PriorityQueue.
+     */
     public PriorityQueue() { }
+
+    /**
+     * The enqueue function takes a given PriorityItem item and places it in
+     * the last position in the array and then bubbles the item up the Priority
+     * Queue until it has reached the proper position.
+     * Inputs:
+     * 1. A PriorityItem item.
+     */
     public void enqueue(PriorityItem item) {
         _items.add(item);
         bubbleUp(item);
     }
 
+    /**
+     * The printList function prints out the character array and priority of
+     * every PriorityItem in the Priority Queue.
+     */
     public void printList() {
         for (PriorityItem item : _items) {
             System.out.println("Item:" + String.valueOf(item.getItem()) + " Priority: " +
@@ -27,6 +37,13 @@ public class PriorityQueue {
         }
     }
 
+    /**
+     * The bubbleDown function is used exclusively by the PriorityQueue class
+     * and takes a PriorityItem and it compares the item to both of its
+     * children and if the given item has a priority value higher than one of
+     * its children it will swap the item with the child and repeat the process
+     * until it does not have a a priority that exceeds one of its children.
+     */
     private void bubbleDown(PriorityItem item) {
         int child1 = _items.indexOf(item)*2+1;
         int child2 = _items.indexOf(item)*2+2;
@@ -48,6 +65,10 @@ public class PriorityQueue {
         }
     }
 
+/**
+ * The bubbleUp function is analogous to the bubbleDown function except it
+ * works in reverse.
+ */
     private void bubbleUp(PriorityItem item) {
 
         if (_items.indexOf(item) != 0) {
@@ -69,15 +90,32 @@ public class PriorityQueue {
         }
     }
 
+    /**
+     * The getSize function returns the size of the Priority Queue.
+     */
     public int getSize() { return _items.size(); }
+
+    /**
+     * The isEmpty function returns true if the Priority Queue is empty,
+     * otherwise it will return false.
+     */
     public boolean isEmpty() { return _items.isEmpty(); }
-    
+
+    /**
+     * The swap function takes 2 PriorityItem items and swaps them within the
+     * PriorityQueue array.
+     */
     private void swap(PriorityItem src, PriorityItem dest) {
         int temp = _items.indexOf(src);
         _items.set(_items.indexOf(dest), src);
         _items.set(temp, dest);
     }
 
+    /**
+     * The dequeue function starts at the beginning of the PriorityQueue array
+     * and removes each item and reorganizes the array until the Priority Queue
+     * is empty.
+     */
     public PriorityItem dequeue() {
         swap(_items.get(0), _items.get(_items.size()-1));
         PriorityItem item = _items.remove(_items.size()-1);
