572e7b219d21c28de6bb83a71cec43ab97bf4480
ryan.relmz32 <ryan.relmz32@66a29046-b2a1-11de-8af0-9d1a786950a5>
2009-10-13 15:13:05 +0000

    Added documentation.

--- trunk/MiniProject/src/miniproject/Logger.java
+++ trunk/MiniProject/src/miniproject/Logger.java
@@ -14,22 +14,37 @@ import java.text.SimpleDateFormat;
 /**
  *
  * @author Raymond Cox <rj.cox101 at gmail.com>
+ * @version 1.0
+ *
+ *
  */
 public class Logger {
+
+    //the name of the file taht we are printing the log to.
     String _fileName;
 
+    /*
+     * The default constructor for the Logger class, creates a log file.
+    */
     public Logger() {
         final String DATE_FORMAT_NOW = "MM-dd-yy HH.mm.ss";
         Date curTime = Calendar.getInstance().getTime();
         _fileName = new SimpleDateFormat(DATE_FORMAT_NOW).format(curTime)+".log";
     }
-
+    /*
+     * Constructor that takes a filename as a parameter.
+    */
     public Logger(String fileName) {
         _fileName = fileName;
     }
-
+    /*
+     * returns the filename of the log file.
+    */
     public String getFileName() { return _fileName; }
 
+    /*
+     * writes the passed String st into the log file.
+    */
     public void write(String st) throws java.io.IOException {
         FileWriter outFile = new FileWriter(_fileName,true);
         PrintWriter logWriter = new PrintWriter(outFile);
